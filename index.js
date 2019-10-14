a7c3cae529c9bc0529a8864480c5629d5740c7e0
JeanneBarriere <barriere.jeanne@gmail.com>
2019-10-14 10:24:48 +0200

    index

--- index.js
+++ index.js
@@ -18,11 +18,3 @@ app.get('/*', function (req, res) {
 app.listen(3000, function () {
   console.log('Application qui écoute sur le port 3000!');
 })
-/*VOTRE TEMPLATE DEFAULTLAYOUT INDEX.HBS
-Créez le fichier index.hbs dans le dossier views, contenant le code suivant :
-LANCER VOTRE PROJET
-Si vous avez installé nodemon, au lieu d'utiliser node index.js, lancez votre projet dans le terminal avec la commande :
-
-nodemon index.js
-De cette façon, le serveur se relancera automatiquement lorsque vous sauvegarderez vos fichiers, et vous pourrez ainsi voir directement les changements*/
-/*npm install -g nodemon --save-dev*/
