4f63f148bc259bc6d5f73c3cd1d04bb057410aff
dreamlumos <kiara.pbelle@gmail.com>
2019-10-15 12:41:23 +0200

    recipe

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -2,7 +2,7 @@ function color(id){
 
 	for (var i=1; i<= id; i++){
 
-		var element = document.getElementById(i);
+		var element = document.getElementById("s"+i);
 		element.classList.remove("empty");
 		element.classList.add("filled");
 		element.innerHTML = "<i class="fas fa-star"></i>";
