a58ed41aaf295d75edcf90132dda28827dc45979
dreamlumos <kiara.pbelle@gmail.com>
2019-10-16 09:43:30 +0200

    recipe

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,3 +1,4 @@
+
 function color(id){
 
 	for (var i=1; i<= id; i++){
@@ -5,8 +6,12 @@ function color(id){
 		var element = document.getElementById("s"+i);
 		element.classList.remove("empty");
 		element.classList.add("filled");
-		element.innerHTML = "<i class="fas fa-star"></i>";
+		element.innerHTML = "<i class='fas fa-star'></i>";
 
 	}
 
 }
+
+
+
+//element.addEventListener("click", color);
