40e7e7de833596577629e3d08eb5e3aac8b8a842
dreamlumos <kiara.pbelle@gmail.com>
2019-10-16 10:34:25 +0200

    recipe

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,4 +1,16 @@
 
+function clickstar(id){
+
+	var element = document.getElementById("s"+id);
+	if (element.classList.contains("empty")){
+		color(id);
+	} else if (element.classList.contains("filled")){
+
+		decolor(id);
+	}
+
+}
+
 function color(id){
 
 	for (var i=1; i<= id; i++){
@@ -12,6 +24,18 @@ function color(id){
 
 }
 
+function decolor(id){
+
+	for (var i = id+1; i <= 5; i++){
+
+		var element = document.getElementById("s"+i);
+		element.classList.remove("filled");
+		element.classList.add("empty");
+		element.innerHTML = "<i class='far fa-star'></i>";
+
+	}
+
+}
 
 
 //element.addEventListener("click", color);
