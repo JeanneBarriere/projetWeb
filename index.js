efebdfe373c0a6bef99ea66f9f3ac094de5fc450
JeanneBarriere <barriere.jeanne@gmail.com>
2019-12-02 18:54:15 +0100

    ...

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.listen(3000, function () {
 
 var pages = require("node-github-pages")(app, {
   static: "public", // Static directory path(css, js...)
-  path: "docs/public" // Output path
+  path: "docs" // Output path
 });
 pages.renderFiles([{
   "view": "index",
