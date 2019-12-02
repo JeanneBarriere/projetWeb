9b686aef4fcb21f37feb55b11678ea7509527e39
JeanneBarriere <barriere.jeanne@gmail.com>
2019-12-02 20:16:27 +0100

    ...

--- docs/js/divers.js
+++
@@ -1,5 +0,0 @@
-function connectforrecipe(){
-  if(window.confirm("Vous n'êtes pas connecté !\n\nVoulez-vous vous connecter pour écrire une nouvelle recette ?")) {
-  			document.location.href="/logIn";
-      };
-};
